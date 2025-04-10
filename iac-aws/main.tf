14ab8175da36e010440f9b0e83bdd64f21d05dc1
Pedro Almeida <pedro22@ua.pt>
2025-04-10 17:08:22 +0100

    Generate key-pair & create security group allowing ssh traffic only

--- iac-aws/main.tf
+++ iac-aws/main.tf
@@ -13,11 +13,50 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_instance" "app_server" {
+resource "aws_security_group" "tr_sg" {
+  name = "tr_sg"
+}
+
+resource "aws_vpc_security_group_egress_rule" "tr_sg_egress" {
+  security_group_id = aws_security_group.tr_sg.id
+  description = "Allow all egress traffic"
+  ip_protocol = "-1"
+  cidr_ipv4 = "0.0.0.0/0"
+}
+
+resource "aws_vpc_security_group_ingress_rule" "tr_sg_ingress" {
+  security_group_id = aws_security_group.tr_sg.id
+  description = "Allow ssh traffic"
+  ip_protocol = "tcp"
+  from_port = 22
+  to_port = 22
+  cidr_ipv4 = "0.0.0.0/0"
+}
+
+
+resource "aws_instance" "tr_instance" {
   ami           = "ami-00a929b66ed6e0de6"
   instance_type = "t2.micro"
+  key_name = aws_key_pair.tr_key.key_name
+  vpc_security_group_ids = [aws_security_group.tr_sg.id]
 
   tags = {
     Name = var.instace_name
   }
+}
+
+resource "tls_private_key" "rsa" {
+  algorithm = "RSA"
+  rsa_bits = 4096
+}
+
+resource "aws_key_pair" "tr_key" {
+  key_name   = "tr_key"
+  public_key = tls_private_key.rsa.public_key_openssh
+}
+
+resource "local_file" "rsa_private_key" {
+  filename = "tr_rsa_key.pem"
+  file_permission = "0600"
+  content = tls_private_key.rsa.private_key_pem
 }
--- iac-aws/outputs.tf
+++ iac-aws/outputs.tf
@@ -1,9 +1,9 @@
 output "instance_id" {
     description = "ID of the EC2 instance"
-    value = aws_instance.app_server.id  
+    value = aws_instance.tr_instance.id  
 }
 
 output "public_ip" {
     description = "Public IP of the EC2 instance"
-    value = aws_instance.app_server.public_ip
+    value = aws_instance.tr_instance.public_ip
 }
